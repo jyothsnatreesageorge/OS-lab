f334ebc8cf9c7b5c6f0c22c3e0107dbc73e17227
Jyothsna Treesa George <168282797+jyothsnatreesageorge@users.noreply.github.com>
2025-04-10 09:47:54 +0530

    Create Calculator.sh
    This is a program intended to build a calculator using shell scripting

---
+++ Calculator.sh
@@ -0,0 +1,29 @@
+$ cat calc.sh
+#!/bin/bash
+echo "MENU"
+echo -e "1.ADD\n2.SUBTRACT\n3.MULTIPLY\n4.DIVIDE\n5.MODULUS\n"
+ch='n'
+while [ "$ch" == "n" ]
+do
+echo "Enter choice:"
+read var
+echo "Enter a:"
+read a
+echo "Enter b"
+read b
+case $var in
+   1)c=$((a+b))
+     echo "$c";;
+   2)c=$((a-b))
+     echo "$c";;
+   3)c=$((a*b))
+     echo "$c";;
+   4)c=$((a/b))
+     echo "$c";;
+   5)c=$((a%b))
+     echo "$c";;
+   *)echo "Invalid Choice";;
+esac
+echo "Do you want to exit?(y/n)?"
+read ch
+done
