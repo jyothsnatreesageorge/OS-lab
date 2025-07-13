ffd552bef6cd8d55a8b090ee2f1c2548405e8bce
Jyothsna Treesa George <168282797+jyothsnatreesageorge@users.noreply.github.com>
2025-07-13 19:48:19 +0530

    Create Chat2.c

---
+++ IPC Shared Memory/Chat2.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<string.h>
+#include<fcntl.h>
+#include<sys/mman.h>
+#include<sys/types.h>
+struct sh
+{
+   int turn,exit;
+   char msg[128];
+};
+int main()
+{
+   int fd=shm_open("/SHARED",O_CREAT|O_RDWR,0644);
+   if(fd==-1) return 1;
+   int fret=ftruncate(fd,sizeof(struct sh));
+   if(fret==-1) return 1;
+   struct sh* s=(struct sh*)mmap(NULL,sizeof(struct sh),PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
+   if(s==MAP_FAILED) return 1;
+   while(1)
+   {
+      if(s->turn==-1)
+      {
+         printf("Chat1 is exiting!\n");
+         break;
+      }
+      while(s->turn==1)
+      {
+         usleep(100);
+      }
+      if(strlen(s->msg)>0)
+      {
+         printf("Chat 1:%s\n",s->msg);
+         memset(s->msg,0,sizeof(s->msg));
+      }
+      printf("Chat 2:");
+      fgets(s->msg,sizeof(s->msg),stdin);
+      s->msg[strcspn(s->msg,"\n")]='\0';
+      if(strcmp(s->msg,"q")==0)
+      {
+         s->turn=-1;
+         break;
+      }
+      s->turn=1;
+   }
+   munmap(s,sizeof(struct sh));
+   shm_unlink("/SHARED");
+   close(fd);
+}
